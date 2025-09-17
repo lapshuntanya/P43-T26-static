38a70aa527717038da6d7fea61822952f734b631
lapshun <lapshun.tatiana@gmail.com>
2025-09-17 17:31:40 +0300

    main

--- P43-T26-static/P43-T26-static.cpp
+++ P43-T26-static/P43-T26-static.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
+#include "CarNumber.h"
+#include <vector>
+
+int CarNumber::counter = 9998;
+char CarNumber::first = 'A';
+char CarNumber::second = 'Z';
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CarNumber::showStatic();
+    cout << "===============================\n\n";
+
+
+    vector<CarNumber> dia;// 0 
+    for (int i = 0; i < 10; i++)
+    {
+        dia.push_back(CarNumber("AX"));
+    }//10
+
+    for (int i = 0; i < dia.size(); i++)
+    {
+        cout << dia[i].getNumber() << "\t";
+        if ((i + 1) % 5 == 0) cout << endl;
+    } 
+    cout << endl;
+
+
+    cout << "===============================\n\n";
+    CarNumber::showStatic();
+
 }
 
  
